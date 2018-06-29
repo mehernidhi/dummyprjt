b0464662c124bad7adc590b2e8893d875557b77d
mehernidhi <mehernidhik@gmail.com>
2018-06-29 18:00:24 +0530

    This is my first commit

---
+++ mainmod.py
@@ -0,0 +1,16 @@
+print('you haves sussfuffly imported the mudle')
+
+a = 1
+
+#def add(a,b):
+	#print(a+b)
+# add(3,4)
+
+
+
+
+#def sub(a,b):
+	#print(a-b)
+
+
+#c =324564
---
+++ modrun.py
@@ -0,0 +1,10 @@
+import mainmod
+# from mainmod import add,sub,a
+from mainmod import *
+
+
+add(1,2)
+
+sub(5,8)
+
+print(a)
